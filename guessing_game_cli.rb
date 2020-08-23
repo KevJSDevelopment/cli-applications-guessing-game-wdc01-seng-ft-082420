2c59b130634bf66e687f4d0e8c945ce19be7936f
Kevin Sheehan <kshhn12@gmail.com>
2020-08-23 13:51:39 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,7 +1,7 @@
 # Code your solution here!
 def run_guessing_game
   rand_num = Random.rand(1...7)
-  puts 'Guess a number between 1 and 6'
+  puts 'Guess a number between 1 and 6\n'
   guess = gets.chomp
   if(guess == rand_num.to_s)
     puts 'You guessed the correct number!'
